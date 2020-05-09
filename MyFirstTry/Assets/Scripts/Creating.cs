483d5765de9dac619cc8cd64b6ed4c0e0f9ee8dd
Poochie18 <39067344+Poochie18@users.noreply.github.com>
2020-05-09 22:43:26 +0300

    Two new Scripts

---
+++ MyFirstTry/Assets/Scripts/Creating.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creating : MonoBehaviour
+{
+    private static Creating instance;
+    public static Creating Instance { get { return instance; } }
+
+    public Tile tile;
+    public Tile[,] tileMap;
+
+    public int height;
+    public int weight;
+
+    public bool mine;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        TilePos();
+    }
+
+    void TilePos()
+    {
+        tileMap = new Tile[height, weight];
+        float size_x = tile.SizeOfx();
+        float size_y = tile.SizeOfy();
+
+        print(size_x);
+        float posX = -size_x * ((float)weight / 2 + 1);
+        float posY = -size_y * ((float)height / 2 - 1);
+
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < weight; j++)
+            {
+                print(posX);
+                posX += size_x;
+                tile.DefaultSkin();
+                tileMap[i, j] = tile.Inizialisation(tile,posX, posY);
+                if (mine = Random.value < 0.3) { tileMap[i,j].isMine = true;}
+            }
+            posY += size_y;
+            posX = -size_x * ((float)weight / 2 + 1);
+        }
+    }
+}
---
+++ MyFirstTry/Assets/Scripts/Tile.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public GameObject tile;
+    public Sprite def;
+    public Sprite mine;
+    public Sprite[] otherTiles;
+    public bool isMine;
+    public bool isClosed;
+
+    /*private static Tile instance;
+    public static Tile Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }*/
+
+    public float SizeOfx()
+    {
+        return tile.GetComponent<BoxCollider2D>().size.x;
+    }
+
+    public float SizeOfy()
+    {
+        return tile.GetComponent<BoxCollider2D>().size.y;
+    }
+
+    public void DefaultSkin()
+    {
+        tile.GetComponent<SpriteRenderer>().sprite = def;
+    }
+
+    public Tile Inizialisation(Tile tile, float pos_x, float pos_y)
+    {
+        return Instantiate(tile, new Vector2(pos_x, pos_y), Quaternion.identity);
+    }
+}
